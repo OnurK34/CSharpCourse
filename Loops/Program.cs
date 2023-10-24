bef3695d0107986df261b7ece9654e5dcc65ca32
Onur <Onur@LAPTOP-L5LHQLLA>
2023-10-25 01:54:48 +0300

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,69 @@
+﻿//ForLoop();
+//WhileLoop();
+//DoWhileLoop();
+//ForEachLoop();
+
+if (IsPrimeNumber(7))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is a not a prime number");
+}
+
+Console.ReadLine();
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (int i = 2; i< number -1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
+static void ForEachLoop()
+{
+    string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+    foreach (var student in students)
+    {
+        Console.WriteLine(student);
+    }
+}
+static void DoWhileLoop()
+{
+    int number = 10;
+    do
+    {
+        Console.WriteLine(number);
+        number--;
+
+    } while (number >= 11);
+}
+static void WhileLoop()
+{
+    int number = 100;
+    while (number >= 0)
+    {
+        Console.WriteLine(number);
+        number--;
+    }
+    Console.WriteLine("Now number is {0}", number);
+}
+
+static void ForLoop()
+{
+    for (int i = 100; i >= 0; i = i - 2)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine("Finished!");
+}
+
+
+
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,31 @@
+﻿// Value Types
+
+//Console.WriteLine("Hello World!");
+
+double number5 = 10.4;    //double = 10
+decimal number6 = 10.4m; //decimal number6 = 10.4M , decimal number6 = 10
+char character = 'A';
+bool condition = false;
+byte number4 = 255; 
+short number3 = 32767;
+int number1 = 2147483647;
+long number2 = 9223372036854775807;
+var number7 = 10;
+number7 = 'A';
+Console.WriteLine("Number1 is {0}",number1);
+Console.WriteLine("Number2 is {0}", number2);
+Console.WriteLine("Number3 is {0}", number3);
+Console.WriteLine("Number4 is {0}", number4);
+Console.WriteLine("Number5 is {0}", number5);
+Console.WriteLine("Number6 is {0}", number6);
+Console.WriteLine("Number7 is {0}", number7);
+Console.WriteLine("Character is : {0}",(int)character);
+Console.WriteLine((int)Days.Friday);
+
+enum Days
+{
+    Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+}
+
+
+
