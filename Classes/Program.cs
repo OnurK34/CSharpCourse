66e0a0f0256fe4e860bb9f8fad352dccf3586909
onurkinatas <kntsonur3428@gmail.com>
2023-10-27 00:24:02 +0300

    added classes project to CSharpCourse solution

---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿Classes.CustomerManager customerManager = new Classes.CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+Console.WriteLine();
+
+Classes.ProductManager productManager = new Classes.ProductManager();
+productManager.Add();
+productManager.Update();
+
+Console.WriteLine();
+
+Classes.Customer customer = new Classes.Customer();
+customer.City = "İstanbul";
+customer.Id = 1;
+customer.FirstName = "Onur";
+customer.LastName = "Kınataş";
+
+Classes.Customer customer2 = new Classes.Customer
+{
+    Id = 2, City = "İzmir", FirstName = "Adem", LastName = "Kınataş"
+};
+
+Console.WriteLine(customer2.FirstName);
+
+
+
