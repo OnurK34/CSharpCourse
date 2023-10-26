6dfb802fcf891ec0884fb38b9b58eb595fb293d5
onurkinatas <kntsonur3428@gmail.com>
2023-10-27 00:25:27 +0300

    added inheritance project to CSharpCourse solution

---
+++ Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿Person[] persons = new Person[3]
+{
+    new Customer{FirstName = "Onur"}, new Student{FirstName = "Adem"}, new Person{FirstName = "Enes"}
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string Lastname { get; set; }
+}
+
+class Customer:Person
+{
+    public string City { get; set; }
+}
+class Student:Person
+{
+    public string Deparment { get; set; }
+}
